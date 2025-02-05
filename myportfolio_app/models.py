07847f1475306fadc8526ca944e5138b6e8670f3
EvG <zhenyagolenko92@gmail.com>
2025-02-05 09:47:27 +0300

    Создал классы моделей

--- myportfolio_app/models.py
+++ myportfolio_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    link = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Сообщение от : {self.name}"
