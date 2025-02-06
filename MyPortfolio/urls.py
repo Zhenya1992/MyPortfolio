a18857a05fadd8cb76e9fbe54924fc8295ec4c7d
EvG <zhenyagolenko92@gmail.com>
2025-02-06 10:23:48 +0300

    Добавил классы представления и написал к ним маршруты

--- MyPortfolio/urls.py
+++ MyPortfolio/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('myportfolio_app.urls', namespace='myportfolio_app')), # myportfolio_app - путь маршрута к файлу(urls.py) глобальный
 ]
