866cd0c7e3e802c26f5b1f5bb1eb360f85a5c756
EvG <zhenyagolenko92@gmail.com>
2025-02-11 11:14:31 +0300

    Создал классы представления проектов, контактов,  html-файлов

--- myportfolio_app/urls.py
+++ myportfolio_app/urls.py
@@ -7,6 +7,8 @@ app_name = 'myportfolio_app'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('projects/', views.ProjectListView.as_view(), name='projects'),
+    path('projects/<int:pk>/', views.ProjectDetailView.as_view(), name='project_detail'),
     path('articles/', views.ArticleListView.as_view(), name='articles'),
     path('contact/', views.ContactView.as_view(), name='contact'),
+    path('add_project/', views.ProjectCreateView.as_view(), name='add_project'),
 ]
